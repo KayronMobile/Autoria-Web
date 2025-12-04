ef0fbb971ac32b3633b21c067d774936e543f960
Kayron(Aniki) <Kayronthegomes13@gmail.com>
2025-12-04 19:02:50 -0300

    teste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListaFrutas } from './Frutas/lista-frutas/lista-frutas';
+import { FormFrutas } from './Frutas/form-frutas/form-frutas';
+export const routes: Routes = [
+  { path: '', component: ListaFrutas },
+  { path: 'cadastro', component: FormFrutas },   
+  { path: 'editar/:id', component: FormFrutas }
 
-export const routes: Routes = [];
+];
